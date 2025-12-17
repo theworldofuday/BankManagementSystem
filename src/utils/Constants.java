0f79e08cac18982b77c3b8a0fbe4452169bc27b9
Uday Kisan Mankar <udayamanakar2004@gmail.com>
2025-12-18 00:03:43 +0530

    Create Constants.java

---
+++ src/utils/Constants.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Constants {
+    public static final String DATA_DIRECTORY = "data/";
+    public static final String USERS_FILE = DATA_DIRECTORY + "users.dat";
+    public static final String ACCOUNTS_FILE = DATA_DIRECTORY + "accounts.dat";
+    public static final String TRANSACTIONS_FILE = DATA_DIRECTORY + "transactions.dat";
+    
+    public static final double MINIMUM_BALANCE = 500.0;
+    public static final double MINIMUM_DEPOSIT = 100.0;
+    public static final double MAXIMUM_WITHDRAWAL = 50000.0;
+    public static final double MAXIMUM_TRANSFER = 100000.0;
+    
+    public static final String ADMIN_ID = "ADMIN001";
+    public static final String ADMIN_PASSWORD = "admin123";
+}
